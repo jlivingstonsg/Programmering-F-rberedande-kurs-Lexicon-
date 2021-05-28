a222f6a7c0156f50bac0c97adb28cb60d8e9fd7c
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-05-28 18:12:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Programmering_Förberedande_kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To C#!");
+        }
+    }
+}
